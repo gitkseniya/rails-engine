930d8f0862c301a070230444ab837d977cfddea3
Kseniya K <kseniya.kudelich@gmail.com>
2021-05-21 21:40:57 -0600

    Modify item factory

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,11 +1,9 @@
 FactoryBot.define do
    factory :item, class: Item do
-     sequence :name do |n|
-       "Item #{n}"
-     end
+
+     name { Faker::TvShows::Suits.character }
      description { Faker::GreekPhilosophers.quote }
      unit_price { Faker::Number.between(from: 3000, to: 100000) }
-     enabled { Faker::Boolean.boolean }
 
      merchant
    end
