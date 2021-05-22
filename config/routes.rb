1508cf27c00970fc49fb20f060834d7bf108d1bd
Kseniya K <kseniya.kudelich@gmail.com>
2021-05-21 21:03:12 -0600

    Handroll non-RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # resources :merchants
-  # resources :items
 
   namespace :api do
     namespace :v1 do
+      get '/merchants/find', to: 'merchants#find'
+      get '/merchants/find_all', to: 'merchants#find_all'
+      get '/items/find', to: 'items#find'
+      get '/items/find_all', to: 'items#find_all'
+
       resources :merchants
+      resources :items
     end
   end
 
