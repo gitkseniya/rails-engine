7452cf672ade47654499c0b14579567a9381392a
Kseniya K <kseniya.kudelich@gmail.com>
2021-05-28 07:27:02 -0600

    Add items index spec

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "items API", type: :request do
+RSpec.describe 'items API', type: :request do
   before :each do
-    create_list(:item, 30)
+    create_list(:item, 51)
   end
 
   describe 'item index' do
-    it "returns all items" do
-
+    it 'returns all items' do
       get '/api/v1/items'
 
-      expect(response).to be_successful
-
       items = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(200)
 
-      expect(items[:data].count).to eq(30)
+      expect(items[:data].count).to eq(20)
 
       items[:data].each do |item|
         expect(item).to have_key(:id)
@@ -27,5 +25,32 @@ RSpec.describe "items API", type: :request do
         expect(item[:attributes][:unit_price]).to be_a(Float)
       end
     end
+
+    it 'returns page 2 with 20 items' do
+      get '/api/v1/items?page=2&per_page=20'
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(200)
+      expect(json[:data].count).to eq(20)
+      expect(json[:data].first[:id].to_i).to be_an(Integer)
+    end
+
+    it 'returns 50 items per page' do
+      get '/api/v1/items?per_page=50'
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(200)
+      expect(json[:data].count).to eq(50)
+      expect(json[:data].first[:id].to_i).to be_an(Integer)
+    end
+
+    it 'returns an empty array if no items' do
+      get '/api/v1/items?page=72&per_page=20'
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(200)
+      expect(json[:data].count).to eq(0)
+    end
+
   end
 end
