896664aa3223f302bc09a68c7996c00190772880
Kseniya K <kseniya.kudelich@gmail.com>
2021-05-27 20:44:17 -0600

    Add sad path for no merchants

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
     @merchant = Merchant.search_by_name(params[:name])
-    render json: MerchantSerializer.new(@merchant)
+
+    if @merchant.nil?
+      render json: { data: {}, error: 'errors' }, status: 200
+    else
+      render json: MerchantSerializer.new(@merchant)
+    end
   end
 end
